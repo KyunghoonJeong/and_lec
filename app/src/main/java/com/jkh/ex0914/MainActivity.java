50bbf37b1bab2b02ff30de79afce44b729eaf526
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-14 15:12:54 +0900

    <update> requestQueue 추가 및 버튼이나 tv et 선언 추가

--- app/src/main/java/com/jkh/ex0914/MainActivity.java
+++ app/src/main/java/com/jkh/ex0914/MainActivity.java
@@ -3,13 +3,27 @@ package com.jkh.ex0914;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue ; //스레드들을 관리하는 객체 !
+    EditText et_input ;
+    TextView tv_result;
+    Button btn_req;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_input =findViewById(R.id.et_input);
+        tv_result = findViewById(R.id.tv_result);
+        btn_req = findViewById(R.id.btn_req);
 
        //volley를 이용한 서버 통신 하기 위한 단계
         //1. 볼리 라이브러리를 추가해야한다.
@@ -20,10 +34,13 @@ public class MainActivity extends AppCompatActivity {
         //2. permission(권한)을 허용 !
         // -> internet 관련 권한 : 인터넷을 통해 data를 주고 받을 수 있도록 ..!
         //매니페스트 폴더 !에서 권한 허용 ! user permission ~
-        //2-1.  프로토콜 허용 !
-        // http , https 둘다 허요
+        //2-1.  프로토콜 허용 !( http프로토콜 사용을 위한)
+        // http , https 둘다 허용
         // android:usesCleartextTraffic="true"  매니페스트에서
         //3. RequestQueue 객체 생성 !!
+
+
+
         //4.Request객체 생성 !
         //5.RequestQueue객체에 Request객체 추가 !
 
