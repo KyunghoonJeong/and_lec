b3d4e73bd42dbf42e12fbbabe204157d6910c2fc
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-14 15:29:59 +0900

    <update> 요청버튼 누르면 리스폰스 받아온 것을 텍스트 뷰 추가해주고 request 객체 생성해줌

--- app/src/main/java/com/jkh/ex0914/MainActivity.java
+++ app/src/main/java/com/jkh/ex0914/MainActivity.java
@@ -3,12 +3,16 @@ package com.jkh.ex0914;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,7 +50,38 @@ public class MainActivity extends AppCompatActivity {
         }
         //리퀘스트큐가 없다면 만들어준다
 
-        //4.Request객체 생성 !
+
+        btn_req.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //요청버튼 누르면 리스폰스를 받아온 것을 텍스트 뷰 추가
+                String name = et_input.getText().toString(); //보낼값 가지고 잇음
+                //보낼 주소 만들기
+                String url = "http://project-student.ddns.net/Android/exam";
+                //현재 우리가 보낼 방식은 ? QueryString 즉 get방식이다.
+                //get방식이기때문에 url 뒤에 ?name= /name 으로 추가 해줌 !
+                url += "?name=";
+                url+= name;
+                //4.Request객체 생성 ! (우리는 StringRequest를 사용 !)
+                //리퀘스트 큐는 4가지 정보를 담아야 한다!  1. 전송방식 (get, post) 2. 어디로 보내야되냐, 요청주소 !  3. 응답 성공 시에 실행되는 로직 4. 응답 실패시
+                StringRequest request = new StringRequest(
+                        Request.Method.GET,  //post방식은 get 대신 post쓰면 된다.
+                        url,
+                        new Response.Listener<String>() {  //응답 성공시
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        },
+                        new Response.ErrorListener() {   //응답 실패시
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                );
+            }
+        });
         //5.RequestQueue객체에 Request객체 추가 !
 
 
