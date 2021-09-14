504e9ab7aa3012f5903aa2f27d4b40ca60540dbd
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-14 16:43:34 +0900

    <update> main4만듬 jason 설명을 위함

---
+++ app/src/main/java/com/jkh/ex0914/MainActivity4_jason.java
@@ -0,0 +1,14 @@
+package com.jkh.ex0914;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4_jason extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity4_jason);
+    }
+}
