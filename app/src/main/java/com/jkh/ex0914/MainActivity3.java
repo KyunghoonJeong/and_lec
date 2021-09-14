26f00f8f9e6bc3ffad4fc6e39a60eb4a175ce150
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-14 16:40:50 +0900

    <update> 5번까지 진행

--- app/src/main/java/com/jkh/ex0914/MainActivity3.java
+++ app/src/main/java/com/jkh/ex0914/MainActivity3.java
@@ -8,7 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity3 extends AppCompatActivity {
@@ -41,15 +45,47 @@ public class MainActivity3 extends AppCompatActivity {
 
 
 
-        //4.Request 객체 생성 (계산버튼 눌렀을때)
+
         btn_cal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                    String num1 = et_num1.getText().toString();
+                    String num2 = et_num2.getText().toString();
+                    //계산 클릭했을 때 두개의 숫자를 받아옴
+
+                String url = "http://project-student.ddns.net/Android/exam2";
+                //이 주소에 ?를 통해서 자료를 넘김
+                url += "?num1=";
+                url += num1;
+                url += "&num2=";
+                url += num2;
+
+                //4.Request 객체 생성 (계산버튼 눌렀을때)  1,2,3,4
+                StringRequest request = new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                tv_cal.setText(response);
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                );
+
+                //5. RequestQueue작업영역에 Request 추가 !
+                requestQueue.add(request);
 
             }
         });
-        //5. RequestQueue작업영역에 Request 추가 !
+
 
     }
 }
