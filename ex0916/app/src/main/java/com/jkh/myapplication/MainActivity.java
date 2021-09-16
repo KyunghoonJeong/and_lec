8ec9f2d46477b5d0f9ac48d4a0246b82577e319d
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-16 14:27:27 +0900

    <update> 기본 틀과 버튼, list뷰 생성 , movielist.xml 생성

--- ex0916/app/src/main/java/com/jkh/myapplication/MainActivity.java
+++ ex0916/app/src/main/java/com/jkh/myapplication/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1. listview 위치
+        //2. item 디자인
+        //3. 아이템 클래스 정의
+        //4. 커스텀어댑터 구현(베이스 어댑터 상속)
+        //5. 어댑터 부착-> 리스트뷰랑
+        //6. 클릭 이벤트(로직)
     }
 }
