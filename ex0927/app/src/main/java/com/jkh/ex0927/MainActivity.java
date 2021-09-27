77d6338edf43dc4dcc4d0bc48d21b9ac4dce5555
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-27 11:13:41 +0900

    <update> 메뉴 버튼 마다 프래그먼트 화면 변경 구현

--- ex0927/app/src/main/java/com/jkh/ex0927/MainActivity.java
+++ ex0927/app/src/main/java/com/jkh/ex0927/MainActivity.java
@@ -36,23 +36,27 @@ public class MainActivity extends AppCompatActivity {
 
                 if(item.getItemId() == R.id.menu1){
                     //지금 선택한 메뉴의 아이디가 menu1이라면
+                    //프래그먼트 갈아끼우는 코드
+                                                                                                                        //프래그먼트가 들어갈 공간 , 지정하고 싶은 프래그먼트 객체.commit() => 반영될 수있도록 commit() 붙여주기
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment1()).commit();
+                            //프래그먼트매니저가 트랜잭션(갈아끼우는)을 하게끔 준비시키는 중 //()에는어디에다 프래그먼트 끼우고싶니 , +  프래그먼트 객체가 들어감
 
                 }
                 else if(item.getItemId() == R.id.menu2){
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment2()).commit();
 
                 }
                 else if(item.getItemId() == R.id.menu3){
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment3()).commit();
 
                 }
                 else if(item.getItemId() == R.id.menu4){
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment4()).commit();
 
                 }
 
 
-                return false;
+                return true;  //리스너의 리턴이 펄스면 아직 끝나지 않았다는 의미가 되어서 다른 ui 작동을 안함
             }
         });
 
